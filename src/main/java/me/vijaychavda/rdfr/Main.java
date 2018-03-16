e2365fbe40b7905cc657dc91c877b27daf889cfe
Vijay Chavda <vijaychavda@gmail.com>
2018-03-16 10:35:18 +0530

    Add Jena dependency and say hello!

---
+++ src/main/java/me/vijaychavda/rdfr/Main.java
@@ -0,0 +1,13 @@
+package me.vijaychavda.rdfr;
+
+/**
+ *
+ * @author Vijay
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
