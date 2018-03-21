d793800d9a6192293ba563e4bbe76c3277777d11
Vijay Chavda <vijaychavda@gmail.com>
2018-03-21 17:14:13 +0530

    Optimize adding meta-data to particular set of statements.

--- src/main/java/me/vijaychavda/rdfr/Main.java
+++ src/main/java/me/vijaychavda/rdfr/Main.java
@@ -1,10 +1,8 @@
 package me.vijaychavda.rdfr;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
@@ -16,48 +14,54 @@ import org.apache.jena.rdf.model.StmtIterator;
 public class Main {
 
             String base = "...";
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) {
 
-        String inputRDF = base + "Q42.nt";
-        String metaRDF = base + "Q42-meta.nt";
-        String reifiedRDF = base + "reified-Q42.nt";
+        String inputPath = base + "Q42.nt";
+        String outputPath = base + "reified-Q42.nt";
+        String format = "NT";
 
-        Model rmodel = Reifier.reify(inputRDF, reifiedRDF);
-        rmodel.write(System.out, "NT");
-
-        System.out.println("\nGAP\n");
-
-        Model mmodel = ModelFactory.createDefaultModel();
-        mmodel.read(metaRDF);
-        mmodel.write(System.out, "NT");
-
-        System.out.println("\nGAP\n");
-
-        Statement statement = rmodel.listStatements().nextStatement();
-        System.out.println(statement.getSubject().getId());
-
-        System.out.println("\nGAP\n");
-
-        String subjectURI = "http://www.wikidata.org/entity/Q42";
-        String propertyURI = "http://www.wikidata.org/prop/direct/P26";
-        String objectURI = "http://www.wikidata.org/prop/direct/Q14623681";
-
-        Resource subject = rmodel.getResource(subjectURI);
-        Property property = rmodel.getProperty(propertyURI);
-        Resource object = rmodel.getResource(objectURI);
-
-        Resource bnode = rmodel.createResource();
-
-        StmtIterator metaStatements = mmodel.listStatements();
-        while (metaStatements.hasNext()) {
-            Statement metaStatement = metaStatements.nextStatement();
-            rmodel.add(bnode, metaStatement.getPredicate(), metaStatement.getObject());
+        Model rmodel = null;
+        try {
+            rmodel = Reifier.reify(inputPath, outputPath, format);
+        } catch (IOException | IllegalArgumentException ex) {
+            System.err.println(ex.getMessage());
+            //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if (rmodel == null) {
+            System.err.println("Reification failed. The program will exit.");
+            System.exit(-1);
         }
 
-        rmodel.add(bnode, property, object);
-        rmodel.add(subject, property, bnode);
+        String metaPath = base + "Q42-meta.nt";
+        String subjectURI = "http://www.wikidata.org/entity/Q42";
+        String propertyURI = "http://www.wikidata.org/prop/direct/P26";
+        String objectURI = "http://www.wikidata.org/entity/Q14623681";
 
-        System.out.println("\nGAP\n");
+        Model model = ModelFactory.createDefaultModel().read(inputPath);
+
+        StmtIterator statements = model.listStatements(
+            model.getResource(subjectURI),
+            model.getProperty(propertyURI),
+            model.getResource(objectURI)
+        );
+
+        StmtIterator metaStatements = ModelFactory.createDefaultModel()
+            .read(metaPath).listStatements();
+
+        Resource meta = rmodel.createResource();
+        while (metaStatements.hasNext()) {
+            Statement metaStatement = metaStatements.nextStatement();
+
+            rmodel.add(meta, metaStatement.getPredicate(),
+                metaStatement.getObject());
+        }
+
+        while (statements.hasNext()) {
+            Statement statement = statements.nextStatement();
+
+            rmodel.add(meta, statement.getPredicate(), statement.getObject());
+            rmodel.add(statement.getSubject(), statement.getPredicate(), meta);
+        }
 
         rmodel.write(System.out, "NT");
     }
